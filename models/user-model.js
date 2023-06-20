f5e4b25fd8fa58ab8e2df38ffba8f9f9009f0bff
nalaso <alans.sunny@gmail.com>
2023-06-20 18:17:07 +0530

    updated model

--- models/user-model.js
+++ models/user-model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    employeeId: {
+    username: {
         type: String,
         required: [true, "Please provide a unique emp_id"],
     },
